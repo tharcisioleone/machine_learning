20f6a2681f608106497aed37c442a410f6ae26b6
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-06-25 13:58:33 +0200

    Update salary.py

--- Salary/salary.py
+++ Salary/salary.py
@@ -4,8 +4,20 @@
 # Importing Libraries
 import numpy as np # linear algebra
 import pandas as pd # data processing
-import matplotlib as mp # data visualisation
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # data visualisation
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# Listing all files under the input directory
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
 
 #Importing the Salary dataset (.csv file)
-data=pd.read_csv('C:\Users\tharc\Documents\Python\Salary/Salary.csv')
+salary_df = pd.read_csv ('/kaggle/input/salary/Salary.csv')
+salary_df
+
+
+#Peeking at top 5 records of dataset
+#data.head()
