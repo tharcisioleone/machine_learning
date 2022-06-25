e7a4a6de6a27c3b6d0bb7fd999180f2c2786daa7
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-06-25 12:20:39 +0200

    Creating Salary

---
+++ Salary/salary.py
@@ -0,0 +1,11 @@
+# Author: Tharcisio Leone #
+# Dataset: Salary #
+
+# Importing Libraries
+import numpy as np # linear algebra
+import pandas as pd # data processing
+import matplotlib as mp # data visualisation
+import matplotlib.pyplot as plt
+
+#Importing the Salary dataset (.csv file)
+data=pd.read_csv('C:\Users\tharc\Documents\Python\Salary/Salary.csv')
