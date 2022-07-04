176bc8daf68230a3c56155b8b4e8253c70ee8f5d
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-04 21:55:32 +0200

    Update send_email.py

--- Gmail/send_email.py
+++ Gmail/send_email.py
@@ -4,7 +4,7 @@
 ## Send mail from your Gmail account using Python
 # 0. Importing Libraries
 # 1. Adding body of email
-# 2. Adding subject, and addresses of sender and receiver.
+# 2. Adding title, and addresses of sender and receiver
 # 3. Including Login Credentials for sending the mail
 # 4. Displaying message confirming the email has been sent.
 
