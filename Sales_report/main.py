8268fc386cd1c28201d7462b780500300635c91a
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-07 18:01:09 +0200

    Update main.py

--- Sales_report/main.py
+++ Sales_report/main.py
@@ -26,4 +26,4 @@ revenue = table_sales[['Store ID', 'Total']].groupby('Store ID').sum() # GroupBy
 print(revenue)
 
 # 4. Calculate the number of products sold per store
-quantidade = tabela_vendas[['Store ID', 'Quanti']].groupby('ID Loja').sum()
+quantidade = table_sales[['Store ID', 'Quantity']].groupby('Store ID').sum()
