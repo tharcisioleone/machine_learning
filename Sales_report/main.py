955782ade2e69f036e1fd809f26986a7eebd4c08
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-09 19:12:49 +0200

    Update main.py

--- Sales_report/main.py
+++ Sales_report/main.py
@@ -20,12 +20,12 @@ table_sales = pd.read_excel('Sales.xlsx')
 # 2. Visualise the data set
 pd.reset_option('display.max_columns', None) # Showing all the columns
 print(table_sales)
-print('-' + 50) # For a better visualisation of the tables
+print('-' * 50) # For a better visualisation of the tables
 
 # 3. Calculate the revenue per store
 revenue = table_sales[['Store ID', 'Total']].groupby('Store ID').sum() # GroupBy + Filter
 print(revenue)
-print('-' + 50) # For a better visualisation of the tables
+print('-' * 50) # For a better visualisation of the tables
 
 # 4. Calculate the number of products sold per store
 quantity = table_sales[['Store ID', 'Quantity']].groupby('Store ID').sum()
