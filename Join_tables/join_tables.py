47e98427616a9c77ec4607f805ce7d8c9402bdbc
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-10 17:55:41 +0200

    Update join_tables.py

---
+++ Join_tables/join_tables.py
@@ -0,0 +1,14 @@
+# Author: Tharcisio Leone #
+# Dataset: Join Tables #
+
+## Join data from two different tables
+# 0. Importing Libraries
+# 1. Reading the data set
+# 2. Visualise the data set
+# 3. Calculate the revenue per store
+# 4. Calculate the number of products sold per store
+# 5. Calculate the average ticket per store
+# 6. Send an email with the sales report
+
+
+# 0. Importing Libraries
