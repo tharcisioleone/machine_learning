53614bdd3cbf33b38cf215ffb6b32ae879ade3a5
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-10 18:30:20 +0200

    Update join_tables.py

--- Join_tables/join_tables.py
+++ Join_tables/join_tables.py
@@ -23,7 +23,7 @@ print('-' * 70)
 
 # 2. Joining both tables
 # See background: https://pandas.pydata.org/docs/user_guide/merging.html#:~:text=pandas%20provides%20various%20facilities%20for,join%20%2F%20merge%2Dtype%20operations.
-table_pytdata = pd.concat([table_python, table_datascience], keys=['Table2', 'Table1']) # Alternativ: ignore_index=True
+table_pytdata = pd.concat([table_python, table_datascience], keys=['Table2', 'Table1']) # Alternative: ignore_index=True
 print(table_pytdata)
 print('-' * 70)
 
