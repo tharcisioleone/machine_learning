ae965eea754f2425a1b6a4ff915b40727277245a
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-12 21:30:34 +0200

    Update credit_card.py

--- Credit_card/credit_card.py
+++ Credit_card/credit_card.py
@@ -1,7 +1,7 @@
 # Author: Tharcisio Leone #
 # Dataset: Credit Card customers #
 
-# Challenge:
+# Challenge: https://www.kaggle.com/datasets/sakshigoyal7/credit-card-customers
 # A manager at the bank is disturbed with more and more customers leaving their credit card services.
 # They would really appreciate if one could predict for them who is gonna get churned. So they can proactively
 # go to the customer to provide them better services and turn customers' decisions in the opposite direction.
@@ -12,3 +12,8 @@
 # 2. Showing most important descriptive statistics of the data
 # 3. Data preparation
 # 4. Creating a data analysis to identify the main reasons for the credit card cancellation
+
+# 0. Importing Libraries
+import pandas as pd
+
+# 1. Reading the data set from Kaggle data
