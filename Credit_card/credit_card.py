54d3b04dc7053ef33399d73fa624129b9e39a021
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-12 21:21:37 +0200

    Update credit_card.py

--- Credit_card/credit_card.py
+++ Credit_card/credit_card.py
@@ -3,14 +3,11 @@
 
 # Challenge:
 # A manager at the bank is disturbed with more and more customers leaving their credit card services.
-# They would really appreciate if one could predict for them who is gonna get churned so they can proactively
+# They would really appreciate if one could predict for them who is gonna get churned. So they can proactively
 # go to the customer to provide them better services and turn customers' decisions in the opposite direction.
 
 ## Maschine Learning using Python
 # 0. Importing Libraries
-# 1. Reading the data set from Kaggle data.
-# 2. Showing the first five rows of the data sets.
-# 3. Calculating the Mean Absolute Error
-# 4. Putting MAE in relation to a basis prediction
-# 5. Including additional variables necessary for the prediction
-# 6. Displaing graphically the bike demand over the day
+# 1. Reading the data set from Kaggle data
+# 2. Showing most important descriptive statistics of the data
+# 3. Creating a data analysis to identify the main reasons for the credit card cancellation
