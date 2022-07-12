ea802fd3a573c16dd26477efbd8d8db1c1894c69
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-12 21:18:18 +0200

    Update credit_card.py

--- Credit_card/credit_card.py
+++ Credit_card/credit_card.py
@@ -1,7 +1,12 @@
 # Author: Tharcisio Leone #
 # Dataset: Credit Card customers #
 
-## Maschine Learning using Linear Regression
+# Challenge:
+# A manager at the bank is disturbed with more and more customers leaving their credit card services.
+# They would really appreciate if one could predict for them who is gonna get churned so they can proactively
+# go to the customer to provide them better services and turn customers' decisions in the opposite direction.
+
+## Maschine Learning using Python
 # 0. Importing Libraries
 # 1. Reading the data set from Kaggle data.
 # 2. Showing the first five rows of the data sets.
