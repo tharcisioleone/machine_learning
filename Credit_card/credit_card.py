be0509243f647e8b55dc321f98d5785fde061160
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-17 23:12:51 +0200

    Update credit_card.py

--- Credit_card/credit_card.py
+++ Credit_card/credit_card.py
@@ -3,8 +3,7 @@
 
 # Challenge: https://www.kaggle.com/datasets/sakshigoyal7/credit-card-customers
 # A manager at the bank is disturbed with more and more customers leaving their credit card services.
-# They would really appreciate if one could predict for them who is gonna get churned. So they can proactively
-# go to the customer to provide them better services and turn customers' decisions in the opposite direction.
+# They would really appreciate if one could predict for them who is gonna get churned. So they can proactivelygo to the customer to provide them better services and turn customers' decisions in the opposite direction.
 
 ## Maschine Learning using Python
 # 0. Importing Libraries
