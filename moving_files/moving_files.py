18630bdce78994c7ee3b57b4e5bc6a570c59b4b1
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-18 22:05:35 +0200

    Raname

--- moving_files/moving_files.py
+++ moving_files/moving_files.py
@@ -1,5 +1,8 @@
 # Author: Tharcisio Leone #
-# Dataset: Cope and past files within the PC  #
+# Dataset: Coping and renaming files within the PC  #
 
 ## Maschine Learning using Linear Regression
-
+# 1. Listing Files
+# 2. Including File Path
+# 3. Renaming Files
+# 4. Copying Files
