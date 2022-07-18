298449635c8833b76702505d4b08fc3dc5a520cb
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-18 22:07:09 +0200

    Update moving_files.py

--- moving_files/moving_files.py
+++ moving_files/moving_files.py
@@ -2,6 +2,7 @@
 # Dataset: Coping and renaming files within the PC  #
 
 ## Maschine Learning using Linear Regression
+# 0. Importing Libraries
 # 1. Listing Files
 # 2. Including File Path
 # 3. Renaming Files
