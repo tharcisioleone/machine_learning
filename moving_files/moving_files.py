7b4082c05c619f7647ddd921001b461e4626d4a1
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-18 21:49:55 +0200

    Importing files

---
+++ moving_files/moving_files.py
@@ -0,0 +1,11 @@
+# Author: Tharcisio Leone #
+# Dataset: Cope and past files within the PC  #
+
+## Maschine Learning using Linear Regression
+# 0. Importing Libraries
+# 1. Reading the data set from Kaggle data.
+# 2. Showing the first five rows of the data sets.
+# 3. Calculating the Mean Absolute Error
+# 4. Putting MAE in relation to a basis prediction
+# 5. Including additional variables necessary for the prediction
+# 6. Displaing graphically the bike demand over the day
