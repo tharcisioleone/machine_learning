8d5bc9f34cc3a8805cc3f89bc4f286367b2408a2
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-19 22:30:20 +0200

    Update moving_files.py

--- moving_files/moving_files.py
+++ moving_files/moving_files.py
@@ -15,24 +15,30 @@
 import os
 import shutil
 
+
 # 1. Listing Files
 files = os.listdir()
 print(files)
 print('x'* 70)
+
 
 # 2. Showing File Path
 path = os.getcwd()
 print(path)
 print('o'* 70)
 
+
 # 3. Renaming Files
 #os.rename('Sales-1.xlsx', 'Sales-1new.xlsx') # To permit the code in line 29
+
 
 # 4. Moving Files
 #os.rename('Sales-1new.xlsx', 'Sales\Sales-1.xlsx') # To permit the code in line 36
 
+
 # 5. Copying Files
 #shutil.copy2('Sales-2.xlsx', 'Sales\Sales-2.xlsx')
+
 
 # 6. Sorting automatically files into the respective folders
 list_files = os.listdir()
