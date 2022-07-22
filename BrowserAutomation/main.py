7d0d23236967fd802de9b039ca4ea08859e7ab1b
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-22 22:43:11 +0200

    Importing Files

---
+++ BrowserAutomation/main.py
@@ -0,0 +1,13 @@
+# Author: Tharcisio Leone #
+# Dataset: Sales Report #
+
+## Create a sales report and send it per e-mail
+# 0. Importing Libraries
+# 1. Reading the data set
+# 2. Visualise the data set
+# 3. Calculate the revenue per store
+# 4. Calculate the number of products sold per store
+# 5. Calculate the average ticket per store
+# 6. Send an email with the sales report
+
+
