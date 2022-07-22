8ada15db63d29d2c3f80598ec7b6810353c2aff1
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-22 23:20:17 +0200

    Update main.py

--- BrowserAutomation/main.py
+++ BrowserAutomation/main.py
@@ -14,21 +14,17 @@
 #pip install selenium
 #pip install webdriver-manager
 
-
 # 1. Import Libraries
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-
 # 2. Install updated version o Chrome Driver
 service = Service(ChromeDriverManager().install())
 browser = webdriver.Chrome(service=service)
 
-
 # 3. Open the homepage of World Bank
 browser.get('https://data.worldbank.org/indicator/SP.POP.TOTL')
-
 
 # 4. Find and download the dataset
 browser.find_element('xpath', '//*[@id="mainChart"]/aside/div/div[2]/div/p/a[1]').click()
