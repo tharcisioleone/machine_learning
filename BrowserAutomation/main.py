51cee5199cf146bb5d0a7129c3036d119955398f
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-22 23:04:36 +0200

    Update main.py

--- BrowserAutomation/main.py
+++ BrowserAutomation/main.py
@@ -17,3 +17,8 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+
+
+servico = Service(ChromeDriverManager().install())
+
+# 2. Open the homepage of World Bank
