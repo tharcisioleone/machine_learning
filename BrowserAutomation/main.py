375099a4f5304e2e20537d9188c2017897cc617c
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-22 22:54:47 +0200

    Update main.py

--- BrowserAutomation/main.py
+++ BrowserAutomation/main.py
@@ -5,11 +5,6 @@
 # 0. Importing selenium and webdriver-manager
 # 1. Importing Libraries
 # 1. Reading the data set
-# 2. Visualise the data set
-# 3. Calculate the revenue per store
-# 4. Calculate the number of products sold per store
-# 5. Calculate the average ticket per store
-# 6. Send an email with the sales report
 
 
 # 0. Importing selenium and webdriver-manager
