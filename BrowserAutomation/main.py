4d73cb46f0ff2a156f0fd3cf4b80b5b265b7d122
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-22 22:54:08 +0200

    Update main.py

--- BrowserAutomation/main.py
+++ BrowserAutomation/main.py
@@ -2,7 +2,8 @@
 # Dataset: Performing browser automation with Selenium #
 
 ## Create an automation to download files in internet
-# 0. Importing Libraries
+# 0. Importing selenium and webdriver-manager
+# 1. Importing Libraries
 # 1. Reading the data set
 # 2. Visualise the data set
 # 3. Calculate the revenue per store
@@ -11,3 +12,7 @@
 # 6. Send an email with the sales report
 
 
+# 0. Importing selenium and webdriver-manager
+#Open Anaconda Prompt(anaconda3)
+#pip install selenium
+#pip install webdriver-manager
