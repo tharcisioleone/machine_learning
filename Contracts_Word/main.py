0f81266da8f7e909534eac20b4a752e10825eeaf
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-23 18:17:38 +0200

    Update main.py

--- Contracts_Word/main.py
+++ Contracts_Word/main.py
@@ -8,7 +8,7 @@
 # 3. Calculate the revenue per store
 # 4. Calculate the number of products sold per store
 # 5. Calculate the average ticket per store
-# 6. Send an email with the sales report
+
 
 
 # 0. Importing Libraries
