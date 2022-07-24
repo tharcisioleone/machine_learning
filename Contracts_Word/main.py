3e994371e6a11f8518e0245675856a27c88fa817
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-24 13:08:39 +0200

    Updating file

--- Contracts_Word/main.py
+++ Contracts_Word/main.py
@@ -8,8 +8,14 @@
 
 
 # 0. Importing Libraries
+import pandas as pd
+import openpyxl as opy
 from docx import Document
 
 # 1. Reading the data set
 document = Document('Contract.docx')
 print(document)
+
+for paragraph in document.paragraphs:
+    print(paragraph.text)
+
