97f6641ca721c9840021bd553f55a39f86c98c1b
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-24 13:45:21 +0200

    Update main.py

--- Contracts_Word/main.py
+++ Contracts_Word/main.py
@@ -59,3 +59,29 @@ document.save(f"Contract - {nome}.docx")
 
 
 # 5. Filling automatically all the items in all contracts
+tabela = pd.read_excel("Informações.xlsx")
+
+for linha in tabela.index:
+    documento = Document("Contrato.docx")
+
+    nome = tabela.loc[linha, "Nome"]
+    item1 = tabela.loc[linha, "Item1"]
+    item2 = tabela.loc[linha, "Item2"]
+    item3 = tabela.loc[linha, "Item3"]
+
+    referencias = {
+        "XXXX": nome,
+        "YYYY": item1,
+        "ZZZZ": item2,
+        "WWWW": item3,
+        "DD": str(datetime.now().day),
+        "MM": str(datetime.now().month),
+        "AAAA": str(datetime.now().year),
+    }
+
+    for paragrafo in documento.paragraphs:
+        for codigo in referencias:
+            valor = referencias[codigo]
+            paragrafo.text = paragrafo.text.replace(codigo, valor)
+
+    documento.save(f"Contrato - {nome}.docx")
