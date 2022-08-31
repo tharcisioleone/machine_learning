e361a4bb19fb5bc2148f39a6b555affd8c940838
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-08-31 18:22:28 +0200

    Update main.py

--- Contracts_Word/main.py
+++ Contracts_Word/main.py
@@ -1,7 +1,7 @@
 # Author: Tharcisio Leone #
 # Dataset: Filling Contract Automatically  #
 
-## Join data from two different 
+## Join data from two
 # 0. Importing Libraries
 # 1. Reading the data sets
 # 2. Visualise the data set
