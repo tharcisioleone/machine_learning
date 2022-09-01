1b3fab674f14ed1c8cff67c8d2fa4bd7c3cb8cca
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-09-01 17:56:03 +0200

    Update main.py

--- Contracts_Word/main.py
+++ Contracts_Word/main.py
@@ -4,7 +4,7 @@
 ## Join data from two different tables
 # 0.
 # 1.
-# 2. Visualise the 
+# 2. Visualise
 # 3. Replace one single item and save it
 # 4. Filling automatically all the items in a single contract
 # 5. Filling automatically all the items in all contracts
