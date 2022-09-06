d300b2f4aeabcbecfa084c36156f1cef20216360
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-09-06 23:32:33 +0200

    Update main.py

--- Contracts_Word/main.py
+++ Contracts_Word/main.py
@@ -1,8 +1,8 @@
 # Author: Tharcisio Leone #
 # Dataset: Filling Contract Automatically  #
 
-## Join data from two different tables
-# 0. Importing
+
+# 0. Importing Libraries
 # 1. Reading the data sets
 # 2. Visualise the data set
 # 3. Replace one single item and save it
